ab9587d42fcba04ff668bcddae772ed92d555cbb
errinjohnson <sporttrac64@outlook.com>
2019-01-23 22:33:57 -0500

    updated website version

--- myJs/myJs.js
+++ myJs/myJs.js
@@ -1,20 +1,21 @@
+
+
 //website last modified 
 var modify = document.lastModified;
+
 var modifyStyle = document.getElementById("lastModified");
 modifyStyle.innerHTML = modify;
 
 
 //clear cache website
-this.addEventListener('activate', function (event) {
-  var cacheWhitelist = ['v2'];
+if(!lastModified)
+{
+  console.log("changes made");
+}
+else
+{
+  console.log("no changes");
+}
+  
+  
 
-  event.waitUntil(
-    caches.keys().then(function (keyList) {
-      return Promise.all(keyList.map(function (key) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-});
